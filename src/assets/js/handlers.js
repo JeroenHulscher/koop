f98a9452f6b6d44ec7756b480905bfa1defd3ac9
Hidde de Vries <hidde@hiddedevries.nl>
2017-09-06 15:38:08 +0200

    various updates to tabs
    - in naming, use 'panel' where actions are about panels (i.e. openTab --> openPanel)
    - move the process to find prev/current/next panels into a separate tabs module for easier reuse
    - add hasButtons variable, and add buttons with prev/next functionality (that are hidden when irrelevant with first/last child selectors)
    
    Signed-off-by: Hidde de Vries <hidde@hiddedevries.nl>

--- src/assets/js/handlers.js
+++ src/assets/js/handlers.js
@@ -9,6 +9,8 @@
 module.exports = {
   'toggle-other-sites': require( 'page-components/header/handlers/toggleOtherSites' ),
   'toggle-nav': require( 'page-components/header/handlers/toggleNav' ),
-  'open-tab': require( 'common/tabs/handlers/openTab' )
+  'open-previous-panel': require( 'common/tabs/handlers/openPrevPanel' ),
+  'open-next-panel': require( 'common/tabs/handlers/openNextPanel' ),
+  'open-panel': require( 'common/tabs/handlers/openPanel' )
 };
 
