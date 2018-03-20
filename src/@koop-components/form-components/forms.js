4919423757a813521d083cb305ea028405da0bcd
Krijn Hoetmer <krijn@qontent.nl>
2018-03-20 15:42:04 +0100

    Keep “Check all” checkboxes in sync

--- src/@koop-components/form-components/forms.js
+++ src/@koop-components/form-components/forms.js
@@ -1,12 +1,35 @@
-onl.handle({
-  'check-all': function( element, event ) {
-    var which = element.getAttribute('data-which');
-    var el = which && document.getElementById( which );
+( function() {
 
-    if ( el ) {
-      onl.dom.$( 'input', el ).forEach( function( input ) {
-        input.checked = element.checked;
-      });
+  'use strict';
+
+  onl.decorate({
+    'check-all': function( element ) {
+      var elements = element.getAttribute('data-for');
+
+      if ( elements ) {
+        elements = onl.dom.$( '[data-set="' + elements + '"]' );
+        if ( elements ) {
+          elements.forEach( function( input ) {
+            input.addEventListener( 'click', function() {
+              var checkedAll = true;
+              elements.forEach( function( input ) {
+                if ( !input.checked ) {
+                  checkedAll = false;
+                }
+              });
+              element.checked = checkedAll;
+            });
+          });
+          element.addEventListener( 'click', function() {
+            var el = this;
+
+            elements.forEach( function( input ) {
+              input.checked = el.checked;
+            });
+          });
+        }
+      }
     }
-  }
-});
+  });
+
+})();
