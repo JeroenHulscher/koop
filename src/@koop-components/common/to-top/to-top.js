77dd9ab8cef54c68ef355458b9ec66353b8b3122
Krijn Hoetmer <krijn@qontent.nl>
2018-04-03 15:23:00 +0200

    Add scroll monitoring to to-top component

---
+++ src/@koop-components/common/to-top/to-top.js
@@ -0,0 +1,21 @@
+( function() {
+
+  'use strict';
+
+  onl.decorate({
+    'to-top': function( element ) {
+      var el = element;
+      el.classList.add('irrelevant');
+      window.addEventListener('scroll', function () {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        if ( scrollTop > 200 ) {
+          el.classList.remove('irrelevant');
+        } else {
+          el.classList.add('irrelevant');
+        }
+      });
+    }
+
+  });
+
+})();
