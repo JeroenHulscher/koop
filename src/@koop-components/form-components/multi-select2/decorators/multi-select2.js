dd27104cd2c07d10de25d1336b5a67aa74cc6497
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2018-07-03 11:46:32 +0200

    massive change; multiselect als component + aria tags

--- src/@koop-components/form-components/multi-select2/decorators/multi-select2.js
+++ src/@koop-components/form-components/multi-select2/decorators/multi-select2.js
@@ -1,1 +1,41 @@
-$('.multipleSelect').fastselect();
+
+
+(function () {
+
+  'use strict';
+
+  onl.decorate({
+    'init-multiselect': function (element) {
+      new multiselect(element);
+    }
+  });
+
+  var multiselect = function (element) {
+    this.element = element;
+    this.config = [];
+
+    this.config.placeholder = this.element.getAttribute( 'data-decorator-placeholder' );
+    this.config.searchPlaceholder = 'Search options';
+    this.config.noResultsText = 'No results';
+    this.config.userOptionPrefix = 'Add ';
+
+    this.init();
+  };
+
+
+  multiselect.prototype.init = function (element) {
+    $('.multipleSelect').fastselect({
+      placeholder: this.config.placeholder,
+      searchPlaceholder: this.config.searchPlaceholder,
+      noResultsText: this.config.noResultsText,
+      userOptionPrefix: this.config.userOptionPrefix
+    });
+    $('.multipleSelect').on('blur', function() {
+      console.log('12');
+      $(this).parents('.fstElement').removeClass('fstResultsOpened');
+      $(this).parents('.fstElement').removeClass('fstActive');
+    });
+
+  };
+
+})();
