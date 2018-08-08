9ac6f5eceeea42b1ce6c28db5967eddd26fc4fb5
Jeroen Hulscher <jeroenhulscher@Macbook.local>
2018-08-08 16:22:26 +0200

    Collapsible: flexible text

--- src/@koop-components/common/collapsible/collapsible.js
+++ src/@koop-components/common/collapsible/collapsible.js
@@ -5,10 +5,16 @@
   var collapsibles = {
     show: function ( collapsible ) {
       onl.dom.$( '.collapsible__header a', collapsible )[0].setAttribute( 'aria-expanded', 'true' );
+      if(onl.dom.$( '.collapsible__header a', collapsible )[0].innerHTML == 'show/hide'){
+        onl.dom.$( '.collapsible__header a', collapsible )[0].innerHTML='Toon minder informatie';
+      }
       onl.ui.show( onl.dom.$( '.collapsible__content', collapsible )[0] );
     },
     hide: function ( collapsible ) {
       onl.dom.$( '.collapsible__header a', collapsible )[0].setAttribute( 'aria-expanded', 'false' );
+      if(onl.dom.$( '.collapsible__header a', collapsible )[0].innerHTML == 'show/hide'){
+        onl.dom.$( '.collapsible__header a', collapsible )[0].innerHTML='Toon meer informatie';
+      }
       onl.ui.hide( onl.dom.$( '.collapsible__content', collapsible )[0] );
     },
     isCollapsed: function ( collapsible ) {
