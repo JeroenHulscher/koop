6e77f6088bc02fe0fa5192ac01f23f733c8914d3
Lesley vd Pol <lesleyvdpol@gmail.com>
2018-11-13 16:39:59 +0100

    Refactor collapse decorator
    It now allows nested decorators and is customizable

--- src/assets/js/decorators/collapse.js
+++ src/assets/js/decorators/collapse.js
@@ -1,24 +1,50 @@
 (function() {
   'use strict';
 
+  var collapse = function(element) {
+    this.element = element;
+    this.config = JSON.parse(this.element.getAttribute('data-config')) || [];
+    this.init();
+  };
+
+  /**
+   * Initialize a new collapse object. Has the possibility to provide a custom trigger in case
+   * any (ore more) of the collapsible are nested.
+   */
+  collapse.prototype.init = function() {
+    // Only apply add event listener logic if a custom trigger was provided.
+    if(typeof this.config.trigger !== 'undefined') {
+      this.addEventListeners(this.element.querySelector('.trigger-collapse.' + this.config.trigger));
+    } else {
+      this.addEventListeners(this.element.querySelector('.trigger-collapse'));
+    }
+  };
+
+  /**
+   * Add event listeners.
+   * @param trigger element used as trigger in the DOM.
+   */
+  collapse.prototype.addEventListeners = function(trigger) {
+    trigger.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.toggleCollapse();
+    }.bind(this), false);
+  };
+
+  /**
+   * Toggle the collapse by applying or removing the `collapsed` class.
+   */
+  collapse.prototype.toggleCollapse = function() {
+    if(this.element.classList.contains('collapsed')) {
+      this.element.classList.remove('collapsed');
+    } else {
+      this.element.classList.add('collapsed');
+    }
+  };
+
   onl.decorate({
-    /**
-     * Applies a 'collapsed' class to the element to collapse after clicking on the trigger.
-     * @param element 
-     * @param event 
-     */
-    'handle-collapse': function(element, event) {
-      var trigger = element.querySelector('.trigger-collapse');
-
-      trigger.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        if(element.classList.contains('collapsed')) {
-          element.classList.remove('collapsed');
-        } else {
-          element.classList.add('collapsed');
-        }
-      });
+    'handle-collapse': function(element) {
+      new collapse(element);
     }
   });
 })();
