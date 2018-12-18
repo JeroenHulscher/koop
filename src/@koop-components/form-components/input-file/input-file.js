42cbd71ba8db0b72e8995bc6d07af7d3fa1a95fd
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2018-12-18 14:28:49 +0100

    alpha + debug mode

--- src/@koop-components/form-components/input-file/input-file.js
+++ src/@koop-components/form-components/input-file/input-file.js
@@ -8,20 +8,25 @@
 
   var inputfile = function( element ) {
     this.element = element;
-    this.label = element.querySelector( 'label' );
-    this.prelabel = element.querySelector( '.js-input-dragbox__prelabel' );
+    this.area = this.element.querySelector( '.js-input-dragbox' );
+    this.label = this.element.querySelector( 'label' );
+    this.prelabel = this.element.querySelector( '.js-input-dragbox__prelabel' );
     this.orginalLabelValue = this.label.innerHTML;
     this.orginalPreLabelValue = this.prelabel.innerHTML;
-    this.input = element.querySelector( 'input' );
-    this.showbuttonClass = '.js-input-dragbox__button';
+    this.input = this.element.querySelector( 'input[type="file"]' );
+    this.buttonReset = this.element.querySelector( '.js-reset' );
+    this.submitbuttonClass = '.js-input-dragbox__button';
     this.config = JSON.parse( this.element.getAttribute( 'data-config' ) ) || [];
+    this.droppedFiles = false;
+    this.debug = this.config.debug;
+    this.responseMsg = this.element.querySelector( '.js-input-dragbox__msg' );
     this.init();
   };
   inputfile.prototype.init = function( ) {
     var self = this;
 
     if (onl.ui.hasDragDrop()) {
-      this.element.classList.add('has-dragdrop');
+      this.area.classList.add('has-dragdrop');
     }
 
     function addListenerMulti(element, eventNames, listener) {
@@ -31,28 +36,35 @@
       }
     }
 
-    addListenerMulti( this.element, 'drag dragstart dragend dragover dragenter dragleave drop', function ( e ) {
+    addListenerMulti( this.area, 'drag dragstart dragend dragover dragenter dragleave drop', function ( e ) {
       e.preventDefault();
       e.stopPropagation();
     });
 
-    addListenerMulti( this.element, 'dragover dragenter', function () {
-      self.element.classList.add( 'is-dragover' );
+    addListenerMulti( this.area, 'dragover dragenter', function () {
+      self.area.classList.add( 'is-dragover' );
     });
 
-    addListenerMulti( this.element, 'dragleave dragend drop', function () {
-      self.element.classList.remove('is-dragover');
+    addListenerMulti( this.area, 'dragleave dragend drop', function () {
+      self.area.classList.remove('is-dragover');
     });
 
-    this.element.addEventListener( 'drop', function( e ) {
+    this.area.addEventListener( 'drop', function( e ) {
+      self.droppedFiles = e.dataTransfer.files;
       self.setAttributes(e.dataTransfer.files);
     });
 
-    this.element.addEventListener( 'change', function( e ) {
+    this.area.addEventListener( 'change', function( e ) {
+      self.droppedFiles = e.target.files;
       self.setAttributes(e.target.files);
     });
 
     this.eventListers();
+
+    // if ( this.config.ajax !== 'undefined' ) {
+      // console.log('this.submitbuttonClass', this.submitbuttonClass);
+      // this.submitbuttonClass.addEventListener('submit', function (e) { e.preventDefault(); e.stopPropagation(); this.submitHandler(e) }.bind(this), false);
+    // }
 
   };
 
@@ -71,7 +83,7 @@
     if (fileName) {
       this.label.querySelector('span').innerHTML = 'Selecteer ander document';
       this.prelabel.innerHTML = fileName;
-      this.element.classList.add('has-file');
+      this.area.classList.add('has-file');
       if (this.config.showbuttonAfterChange) {
         this.showbuttonAfterChange();
       }
@@ -79,7 +91,7 @@
     else {
       this.label.innerHTML = this.orginalLabelValue;
       this.prelabel.innerHTML = this.orginalPreLabelValue;
-      this.element.classList.remove('has-file');
+      this.area.classList.remove('has-file');
       if (this.config.showbuttonAfterChange) {
         this.hidebuttonAfterChange();
       }
@@ -87,10 +99,15 @@
   };
 
   inputfile.prototype.showbuttonAfterChange = function() {
-    document.querySelector( this.showbuttonClass ).removeAttribute('hidden');
+    this.element.querySelector( this.submitbuttonClass ).removeAttribute('hidden');
   };
   inputfile.prototype.hidebuttonAfterChange = function() {
-    document.querySelector( this.showbuttonClass ).addAttribute('hidden');
+    this.element.querySelector( this.submitbuttonClass ).addAttribute('hidden');
+  };
+
+  inputfile.prototype.resetElement = function() {
+    this.element.querySelector('.js-input-dragbox').classList.remove('is-success');
+    this.element.querySelector('.js-input-dragbox').classList.remove('is-uploading');
   };
 
   inputfile.prototype.eventListers = function() {
@@ -101,9 +118,87 @@
       }
     });
 
+    this.buttonReset.addEventListener('click', function ( e ) { this.resetElement(e) }.bind( this ), false );
+
+
     // Firefox bug fix
-    this.element.addEventListener('focus', function () { element.classList.add('has-focus'); });
-    this.element.addEventListener('blur', function () { element.classList.remove('has-focus'); });
+    this.area.addEventListener('focus', function ( e ) { this.area.classList.add('has-focus'); }.bind( this ), false );
+    this.area.addEventListener('blur', function ( e ) { this.area.classList.remove('has-focus'); }.bind( this ), false );
+
+    this.element.addEventListener('submit', function (e) {
+      console.log('submit');
+      this.ajax = new XMLHttpRequest();
+      var errorMsg;
+      var ajaxData = new FormData( this.element );
+
+      if (this.area.classList.contains('is-uploading')) return false;
+
+      this.area.classList.add('is-uploading');
+      this.area.classList.remove('is-error');
+
+      if ( onl.ui.hasDragDrop() ) {
+        e.preventDefault();
+
+        // gathering the form data
+        if( this.droppedFiles ) {
+          Array.prototype.forEach.call( this.droppedFiles, function( file ) {
+            ajaxData.append( this.input.getAttribute( 'name' ), file );
+          }.bind(this));
+        }
+
+        if (!this.debug) {
+          this.ajax.upload.addEventListener( 'progress', function (e) { this.progressHandler(e); }.bind(this), false );
+          this.ajax.upload.addEventListener( 'load', function (e) { this.loadHandler(e); }.bind(this), false );
+          this.ajax.upload.addEventListener( 'error', function (e) { this.errorHandler(e); }.bind(this), false );
+          this.ajax.open( this.element.getAttribute( 'method' ), this.element.getAttribute( 'action' ), true );
+          this.ajax.send( ajaxData );
+        } else {
+          this.ajax = new Array({
+            status: 300,
+            responseText: {
+              success: true,
+              message: 'Bestand is succesvol geupload'
+            }
+          });
+          this.ajax = this.ajax[0];
+          // this.ajax.status = 300;
+          // this.ajax.responseText.success = true;
+          this.loadHandler();
+        }
+
+      }
+
+    }.bind(this), false);
+
+  };
+
+  inputfile.prototype.loadHandler = function (e) {
+    this.area.classList.remove( 'is-uploading' );
+    if (this.ajax.status >= 200 && this.ajax.status < 400 )
+    {
+      if ( this.debug && this.ajax.responseText.success ) {
+        this.area.classList.add( this.ajax.responseText.success == true ? 'is-success' : 'is-error' );
+        this.responseMsg.innerHTML = this.ajax.message;
+      } else {
+        var data = JSON.parse( this.ajax.responseText );
+        this.area.classList.add( data.success == true ? 'is-success' : 'is-error' );
+        this.responseMsg.innerHTML = this.ajax.message;
+      }
+
+    }
+
+  };
+
+  inputfile.prototype.errorHandler = function (e) {
+    this.element.classList.remove( 'is-uploading' );
+    this.responseMsg.innerHTML = 'Document kan niet worden geupload';
+  };
+
+  inputfile.prototype.progressHandler = function(event) {
+    var percent = (event.loaded / event.total) * 100;
+
+    this.element.querySelector('.input-dragbox__uploading__msg span').innerHTML = Math.round(percent);
+    this.element.querySelector('.input-dragbox__uploading__progress').value = Math.round(percent);
   };
 
 })();
