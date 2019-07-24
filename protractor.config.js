6f90da38a2e7072054bdd960f6e58b18a05f2a3b
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2019-07-24 11:57:57 +0200

    updated protractor

--- protractor.config.js
+++ protractor.config.js
@@ -1,8 +1,8 @@
 var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 exports.config = {
     framework: 'jasmine',
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
-    seleniumAddress: './node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    // seleniumAddress: './node_modules/webdriver-manager/selenium/selenium-server-standalone-3.141.59.jar',
     specs: ['**/*.e2e.js'],
 
     onPrepare: function() {
