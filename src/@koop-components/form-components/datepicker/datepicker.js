9995fc3e04e35b7af221473052ebd4d8d999219a
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2020-06-15 18:36:19 +0200

    now also trigger change event when date selected from datepicker

--- src/@koop-components/form-components/datepicker/datepicker.js
+++ src/@koop-components/form-components/datepicker/datepicker.js
@@ -25,7 +25,7 @@
       this.initDatepicker( element );
 
       $(element).on('change', function () {
-        this.setAttribute('data-date', moment(this.value, 'YYYY-MM-DD').format(this.getAttribute('data-date-format')));
+        this.setAttribute('data-date', this.value);
         self.hiddenfield.setAttribute('value', this.value);
         if ( self.round > 0 ) {
           if ($('.js-fixedbottom-button__button')){
@@ -36,8 +36,8 @@
       }).trigger('change');
     } else {
       $(element).on('change', function () {
-        this.setAttribute('data-date', moment(this.value, 'YYYY-MM-DD').format(this.getAttribute('data-date-format')));
-        self.hiddenfield.setAttribute('value', moment(this.value, 'YYYY-MM-DD').format(this.getAttribute('data-date-format')));
+        this.setAttribute('data-date', moment(this.value, 'YYYY-MM-DD').format("DD-MM-YYYY"));
+        self.hiddenfield.setAttribute('value', moment(this.value, 'YYYY-MM-DD').format("DD-MM-YYYY"));
         if (self.round > 0) {
           if ($('.js-fixedbottom-button__button')) {
             $('.js-fixedbottom-button__button').removeAttr('hidden');
@@ -81,6 +81,7 @@
         if ($('.js-fixedbottom-button__button')) {
           $('.js-fixedbottom-button__button').removeAttr('hidden');
         }
+        $(this).change();
       }
     });
     $(element).val($(element).data('date'));
