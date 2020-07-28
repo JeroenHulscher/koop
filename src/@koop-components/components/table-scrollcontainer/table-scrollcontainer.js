3ada1a5fc8fbef4350f1c19734b0587348df0e52
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2020-07-28 15:22:24 +0200

    scroll table fix on pageload (now also checks height on non-excisting table-containers)

--- src/@koop-components/components/table-scrollcontainer/table-scrollcontainer.js
+++ src/@koop-components/components/table-scrollcontainer/table-scrollcontainer.js
@@ -10,41 +10,46 @@
   var tableContainer = function () {
     var allTables = document.getElementsByTagName('table');
     var i;
+    var container = false;
 
     for (i = 0; i < allTables.length; i++) {
-      if (!allTables[i].parentNode.classList.contains('table__container')) {
+      if ((!allTables[i].parentNode.classList.contains('table__container')) && (!allTables[i].parentNode.classList.contains('table--container'))) {
         var div = document.createElement('div');
-        var container = div.cloneNode(false);
+        container = div.cloneNode(false);
 
         allTables[i].parentNode.insertBefore(container, allTables[i]);
         container.classList.add('table__container');
         container.appendChild(allTables[i]);
+      }
+      if(!container) {
+        container = allTables[i].parentNode;
+      }
+      var windowHeight = window.innerHeight;
+      var tableHeight = allTables[i].offsetHeight;
+      var tableWidth = allTables[i].offsetWidth;
+      var containerWidth = container.offsetWidth;
 
-        var windowHeight = window.innerHeight;
-        var tableHeight = allTables[i].offsetHeight;
-        var tableWidth = allTables[i].offsetWidth;
-        var containerWidth = container.offsetWidth;
+      // new table height is 80% of the current viewport height
+      var newContainerHeight = windowHeight - (windowHeight * 0.2);
 
-        // new table height is 80% of the current viewport height
-        var newContainerHeight = windowHeight - (windowHeight * 0.2);
-
-        // check if the table is wider than it's container
-        if (containerWidth < tableWidth) {
-          container.classList.add('has-overflow');
-          // check if the table if bigger (in height) than the viewport
-          if (tableHeight >= windowHeight) {
-            container.style.height = newContainerHeight + 'px';
-          }
+      // check if the table is wider than it's container
+      if (containerWidth < tableWidth) {
+        container.classList.add('has-overflow');
+        // check if the table if bigger (in height) than the viewport
+        if (tableHeight >= windowHeight) {
+          container.style.height = newContainerHeight + 'px';
         }
       }
+
     }
   }
   var recalculateTableContainerHeight = function () {
-    var allFixedTableContainers = document.querySelectorAll('.table__container');
+    var allFixedTableContainers = document.querySelectorAll('.table--container, .table__container');
     var i;
 
     for (i = 0; i < allFixedTableContainers.length; i++) {
       var windowHeight = window.innerHeight;
+      var tableHeight = allFixedTableContainers[i].querySelector('table').offsetHeight;
       var tableWidth = allFixedTableContainers[i].querySelector('table').offsetWidth;
       var containerWidth = allFixedTableContainers[i].offsetWidth;
 
@@ -53,14 +58,19 @@
 
       // check if the table is wider than it's container
       if (containerWidth < tableWidth) {
-        allFixedTableContainers[i].classList.add('has-overflow');
-        allFixedTableContainers[i].style.height = newContainerHeight + 'px';
+        // check if the table if bigger (in height) than the viewport
+        if (tableHeight >= windowHeight) {
+          allFixedTableContainers[i].classList.add('has-overflow');
+          allFixedTableContainers[i].style.height = newContainerHeight + 'px';
+        }
       } else {
         allFixedTableContainers[i].classList.remove('has-overflow');
         allFixedTableContainers[i].style.height = 'auto';
       }
     }
   }
+
+  // activate scroll;
   tableContainer();
 
   window.addEventListener('resize', function () {
