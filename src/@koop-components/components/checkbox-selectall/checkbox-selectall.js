b8d6fb1f9eb752343bb67b911e9a0b0648fd43d3
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2020-09-01 13:46:19 +0200

    checkbox select all; ‘selectall’ ook checked na selecteren alle checkboxes

--- src/@koop-components/components/checkbox-selectall/checkbox-selectall.js
+++ src/@koop-components/components/checkbox-selectall/checkbox-selectall.js
@@ -11,65 +11,94 @@
     this.element = element;
     this.mastercheckbox = this.element.querySelector('.js-checkbox-master');
     this.config = JSON.parse(this.element.getAttribute('data-config')) || [];
-    this.checkboxes = this.element.querySelectorAll('input[type="checkbox"]');
+    this.checkboxes = this.element.querySelectorAll('input[type="checkbox"]:not(.js-checkbox-master)');
 
     this.amountLabel = this.element.querySelector('.js-amount-checkboxes');
     if(this.amountLabel){
-      this.amountLabel.innerHTML = this.checkboxes.length - 1;
+      this.amountLabel.innerHTML = this.checkboxes.length;
     }
 
-    this.initEventListeners();
+    this.init();
   };
+
+  selectall.prototype.init = function (e) {
+    // check mastercheckbox if all checkboxes are checked on pageload;
+    if(this.areAllCheckboxesChecked()){
+      this.mastercheckbox.checked = true;
+    }
+    this.initEventListeners();
+  }
 
   selectall.prototype.initEventListeners = function (e) {
     var i;
 
-    // master checkbox (select all)
-    this.mastercheckbox.addEventListener('change', function (e) {
-      var i;
-      var checkboxes = this.checkboxes;
-
-      for (i = 0; i < checkboxes.length; i++) {
-        if (checkboxes[i] !== e.target) {
-
-          // skip this checkbox if it's not visible;
-          if(!onl.ui.isVisible(checkboxes[i])) {
-            continue;
-          }
-
-          checkboxes[i].checked = e.target.checked;
-
-          // onchange event needs manual triggering on checkboxes
-          if ("createEvent" in document) {
-            var evt = document.createEvent("HTMLEvents");
-            evt.initEvent("change", false, true);
-            checkboxes[i].dispatchEvent(evt);
-          } else {
-            checkboxes[i].fireEvent("onchange");
-          }
-        }
-
-      }
-    }.bind(this), false);
-
-    // uncheck the 'select all'-checkbox when any of the checkboxes is not checked anymore;
+    // regular checkboxes;
     for (i = 0; i < this.checkboxes.length; i++) {
-      this.checkboxes[i].addEventListener('change', function (e) {
-        if (!e.target.classList.contains('js-checkbox-master')) {
-          this.regularCheckboxListener(e);
-        }
-      }.bind(this), false);
+      this.checkboxes[i].addEventListener('click', function (e) { this.changeCheckbox(e); }.bind(this), false);
     }
 
+    // master checkbox (select all)
+    this.mastercheckbox.addEventListener('click', function (e) { this.changeMasterCheckbox(e); }.bind(this), false);
   };
 
-  selectall.prototype.regularCheckboxListener = function (e) {
-    var checkbox = e.target;
+  selectall.prototype.areAllCheckboxesChecked = function (e) {
+    if (this.countAmountChecked() === this.checkboxes.length) {
+      return true;
+    }
+    return false;
+  }
+  selectall.prototype.countAmountChecked = function (e) {
+    var y;
+    var amountChecked = 0;
 
-    if (checkbox.checked === false && this.mastercheckbox.checked) {
+    // count amount of checked checkboxes
+    for (y = 0; y < this.checkboxes.length; y++) {
+      if (this.checkboxes[y].checked) {
+        amountChecked++;
+      }
+    }
+    return amountChecked;
+  }
+
+  selectall.prototype.changeCheckbox = function (e) {
+    var totalCheckboxes = this.checkboxes.length;
+
+    // after un-checking the current checkbox, check the mastercheckbox and if needed uncheck that one as well.
+    if (e.target.checked === false && this.mastercheckbox.checked) {
       this.mastercheckbox.checked = false;
     }
 
+    // if all checkboxes are checked, also check the mastercheckbox.
+    if (totalCheckboxes === this.countAmountChecked()) {
+      this.mastercheckbox.checked = true;
+    }
+  }
+
+  selectall.prototype.changeMasterCheckbox = function (e) {
+    var i;
+    var checkboxes = this.checkboxes;
+
+    for (i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i] !== e.target) {
+
+        // skip this checkbox if it's not visible;
+        if(!onl.ui.isVisible(checkboxes[i])) {
+          continue;
+        }
+
+        checkboxes[i].checked = e.target.checked;
+
+        // onchange event needs manual triggering on checkboxes
+        if ("createEvent" in document) {
+          var evt = document.createEvent("HTMLEvents");
+          evt.initEvent("change", false, true);
+          checkboxes[i].dispatchEvent(evt);
+        } else {
+          checkboxes[i].fireEvent("onchange");
+        }
+      }
+
+    }
   };
 
 })();
