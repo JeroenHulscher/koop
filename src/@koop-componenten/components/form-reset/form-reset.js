408770eb02fddbd85ffc49c4bf827b6adf852365
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2020-09-11 11:23:23 +0200

    form reset; nu mogelijk om het type element zelf te configureren

--- src/@koop-componenten/components/form-reset/form-reset.js
+++ src/@koop-componenten/components/form-reset/form-reset.js
@@ -10,9 +10,12 @@
   var formReset = function (element) {
     this.element = element;
     this.config = JSON.parse(this.element.getAttribute('data-config')) || [];
+
+    this.elements = this.config.elements || 'input, select';
     this.resetElementClass = this.config.resetElementClass || 'formreset-resetlink';
 
     this.resetLink = this.element.querySelector('.' + this.resetElementClass);
+
     if (this.resetLink) {
       this.initEventListeners();
     }
@@ -24,35 +27,62 @@
 
   formReset.prototype.resetForm = function(e) {
     var y;
+    var type;
 
-    this.inputs = this.element.querySelectorAll('input,select');
+    this.inputs = this.element.querySelectorAll(this.elements);
 
     for (y = 0; y < this.inputs.length; y++) {
 
-      switch (this.inputs[y].getAttribute('type')) {
-      case 'radio':
-        if (this.inputs[y].checked) {
-          this.inputs[y].checked = false;
-        }
-      case 'checkbox':
-        if (this.inputs[y].checked) {
-          this.inputs[y].checked = false;
-        }
-      case 'text':
-        this.inputs[y].value = '';
-          // trigger keyUp event on input (for ie. filtersearch-results component)
-          if ("createEvent" in document) {
-            var evt = document.createEvent("HTMLEvents");
-            evt.initEvent("keyup", false, true);
-            this.inputs[y].dispatchEvent(evt);
-          } else {
-            this.inputs[y].fireEvent("keyup");
+      // get type
+      if (this.inputs[y].nodeName === 'INPUT') {
+        type = this.inputs[y].getAttribute('type')
+      } else if (this.inputs[y].nodeName === 'SELECT') {
+        type = 'select';
+      }
+
+      if (type) {
+        switch (type) {
+        case 'radio':
+          if (this.inputs[y].checked) {
+            this.inputs[y].checked = false;
           }
+        case 'checkbox':
+          if (this.inputs[y].checked) {
+            this.inputs[y].checked = false;
+          }
+        case 'select':
+          this.inputs[y].selectedIndex = 0;
+          this.fireEvent(this.inputs[y], 'change');
+        case 'text':
+          this.inputs[y].value = '';
+            // trigger keyUp event on input (for ie. filtersearch-results component)
+            if ("createEvent" in document) {
+              var evt = document.createEvent("HTMLEvents");
+              evt.initEvent("keyup", false, true);
+              this.inputs[y].dispatchEvent(evt);
+            } else {
+              this.inputs[y].fireEvent("keyup");
+            }
+        }
       }
     }
 
     e.preventDefault();
   };
+
+  formReset.prototype.fireEvent = function(element, event) {
+    if (document.createEventObject) {
+      // dispatch for IE
+      var evt = document.createEventObject();
+      return element.fireEvent('on' + event, evt)
+    }
+    else {
+      // dispatch for firefox + others
+      var evt = document.createEvent("HTMLEvents");
+      evt.initEvent(event, true, true); // event type,bubbling,cancelable
+      return !element.dispatchEvent(evt);
+    }
+  }
 
 
 
