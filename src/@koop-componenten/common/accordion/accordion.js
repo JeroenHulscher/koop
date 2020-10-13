2486d19c1ccc9e9066f18ddc872b680a85a7379c
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2020-10-13 09:04:48 +0200

    accordion type checkbox v0.1

--- src/@koop-componenten/common/accordion/accordion.js
+++ src/@koop-componenten/common/accordion/accordion.js
@@ -14,9 +14,12 @@
     this.allowMultiplePanelsOpen = this.config.allowMultiplePanelsOpen || true;
     this.allowToggle = this.config.allowToggle || true;
 
+    this.accordionHeaderClass = this.config.accordionHeaderClass || 'accordion__item__header';
     this.triggerClass = this.config.triggerClass || 'accordion__item__header-trigger';
+    this.checkboxTriggerClass = this.config.checkboxTriggerClass || 'is-accordion-trigger';
 
     this.triggers = this.element.querySelectorAll('.' + this.triggerClass);
+    this.checkboxTriggers = this.element.querySelectorAll('.' + this.checkboxTriggerClass);
 
     this.init();
     this.initEventListeners();
@@ -34,16 +37,42 @@
 
   accordion.prototype.initEventListeners = function () {
     var i;
+    var y;
+
+    for (y = 0; y < this.checkboxTriggers.length; y++) {
+      this.checkboxTriggers[y].addEventListener('click', function (e) { this.doCheckboxTriggerAction(e); }.bind(this), false);
+    }
 
     for (i = 0; i < this.triggers.length; i++) {
-      this.triggers[i].addEventListener('click', function (e) { this.doTriggerAction(e); }.bind(this), false);
+      this.triggers[i].addEventListener('click', function (e) { this.doTriggerAction(e.target); }.bind(this), false);
       this.triggers[i].addEventListener('focus', function () { this.element.classList.add('is-focused'); }.bind(this), false);
       this.triggers[i].addEventListener('blur', function () { this.element.classList.remove('is-focused'); }.bind(this), false);
     }
   };
 
+  accordion.prototype.doCheckboxTriggerAction = function (e) {
+    console.log('e',e);
+    console.log('e',e.target.checked);
+
+    function findAncestor(el, cls) {
+      while ((el = el.parentElement) && !el.classList.contains(cls));
+      return el;
+    }
+
+    // if(e.target.checked) {
+      var header = findAncestor(e.target, this.accordionHeaderClass);
+      console.log('header', header);
+      console.log('this.triggerClass', this.triggerClass);
+      var trigger = header.querySelector('.' + this.triggerClass);
+      if (trigger) {
+        trigger.click();
+      }
+
+    // }
+  }
+
   accordion.prototype.doTriggerAction = function (e) {
-    var trigger = e.target;
+    var trigger = e;
 
     function findAncestor(el, cls) {
       while ((el = el.parentElement) && !el.classList.contains(cls));
@@ -82,7 +111,7 @@
         document.getElementById(trigger.getAttribute('aria-controls')).setAttribute('aria-hidden', 'true');
       }
     }
-    e.preventDefault();
+    // e.preventDefault();
   };
 
 })();
