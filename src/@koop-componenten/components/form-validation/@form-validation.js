34842a9efbea1fb4a13cdf136582a38ce40744c6
Ruurd van Veelen <ruurdvanveelen@gmail.com>
2021-03-10 11:20:06 +0100

    wip form validation setup

---
+++ src/@koop-componenten/components/form-validation/@form-validation.js
@@ -0,0 +1,32 @@
+/**
+ * Element.matches() polyfill (simple version)
+ * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
+ */
+if (!Element.prototype.matches) {
+  Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+/**
+ * Feature test
+ * @return {Boolean} Returns true if required methods and APIs are supported by the browser
+ */
+var supports = function () {
+  if (!document.addEventListener && !document.querySelector('body')) {
+    return false;
+  }
+  return true;
+};
+
+(function () {
+  'use strict';
+
+  onl.decorate({
+    //'init-form-validation': function (element) {
+      // new formvalidation(element);
+    //}
+  });
+
+})();
+/*
+Inspired by Go Make Things, LLC's project "validate", MIT, https://github.com/cferdinandi/validate
+*/
